b0c3dd6351f0584381aa8abbddb66f63a6a73a65
jhimhope <jhimhope@yahoo.com>
2026-04-15 14:07:03 +0800

    Fix: Add missing SearchBar import to Navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link'
 import { User, ChevronDown, Package, Settings, LogOut } from 'lucide-react'
 import { useAuth } from './AuthProvider'
 import CartIcon from './CartIcon'
+import SearchBar from './SearchBar'
 
 export default function Navbar() {
   const { user, signOut } = useAuth()
